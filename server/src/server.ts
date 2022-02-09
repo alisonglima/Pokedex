27ba2a9f45b04b577ee67ad2a88949b3a4d3b8a5
Alison Gustavo de Lima <alisonlima2@hotmail.com>
2022-02-09 16:58:08 -0300

    fix: server logging message

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import App from "./app";
 
 App.listen(App.get("port"), () => {
-  console.log(`Blog API is listening on port ${App.get("port")}`);
+  console.log(`Pokédex API is listening on port ${App.get("port")}`);
 });
