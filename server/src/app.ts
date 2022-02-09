cf01030387775fd854b517dca9bf73181fe79f55
Alison Gustavo de Lima <alisonlima2@hotmail.com>
2022-02-09 17:01:14 -0300

    fix: server variables name

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,25 +1,25 @@
 if (process.env.NODE_ENV === "test") require("dotenv").config();
-import ex from "express";
+import express from "express";
 import helmet from "helmet";
 import { json, urlencoded } from "body-parser";
 
-function express() {
-  const app = ex();
+function app() {
+  const server = express();
 
-  app.use(helmet({ hidePoweredBy: true }));
-  app.use(urlencoded({ extended: true }));
-  app.use(json());
+  server.use(helmet({ hidePoweredBy: true }));
+  server.use(urlencoded({ extended: true }));
+  server.use(json());
 
-  // app.use(process.env.API_PREFIX || "/v1", routes);
+  // server.use(process.env.API_PREFIX || "/v1", routes);
 
-  app.get("/health", (_, res) => {
+  server.get("/health", (_, res) => {
     res.json({ message: "Server is running :D" });
     res.status(200);
   });
 
-  app.set("port", process.env.PORT || 3333);
+  server.set("port", process.env.PORT || 3333);
 
-  return app;
+  return server;
 }
 
-export default express();
+export default app();
